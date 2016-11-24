8410e06aa94c23f498e3cf7b21cc4262929574bf
Metallica88 <EmanuelLangbauer@yahoo.de>
2016-11-24 18:47:15 +0100

    Test

--- src/myFirstIntellijTest.java
+++ src/myFirstIntellijTest.java
@@ -6,6 +6,7 @@ public class myFirstIntellijTest {
     public static void main(String [ ] args) {
         System.out.println("Hallo, Welt!");
         System.out.println("Hallo, Du!");
+        System.out.println("Auch Hallo");
     }
 
 }
