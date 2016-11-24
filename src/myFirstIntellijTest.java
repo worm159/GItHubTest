b879dcba2b5729f8965d84e2342754c2409c0c2b
worm159 <august.wurm@sport-hartkirchen.at>
2016-11-24 18:44:51 +0100

    Test01

--- src/myFirstIntellijTest.java
+++ src/myFirstIntellijTest.java
@@ -5,6 +5,7 @@ public class myFirstIntellijTest {
 
     public static void main(String [ ] args) {
         System.out.println("Hallo, Welt!");
+        System.out.println("Hallo, Du!");
     }
 
 }
